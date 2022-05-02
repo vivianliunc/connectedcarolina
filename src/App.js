e2dd56c77f4aa110ce50478bb965ca7f6f5c8712
pranavchintal <44687141+pranavchintal@users.noreply.github.com>
2022-05-02 13:22:23 -0400

    nav routing implemented, create group page mostly done, minor groupfeed refinements

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles.css';
+import './App.css';
+import GroupFeed from './pages/GroupFeed';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>h1</h1>
+      <GroupFeed />
+    </div>
+  );
+}
+
+export default App;
